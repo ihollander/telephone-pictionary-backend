8fdfb6adb9fc831664723b9db306ad3facf521ef
ihollander <ianholla@gmail.com>
2019-05-05 14:01:42 -0400

    game routes

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,21 +3,36 @@ const mongoose = require("mongoose");
 const Game = mongoose.model("games");
 
 module.exports = app => {
+  app.get("/api/games", async (req, res) => {
+    const game = await Game.find({}); // find all
+
+    res.json(game);
+  });
+
   app.get("/api/games/:id", async (req, res) => {
     const { id } = req.params;
 
     const game = await Game.findById(id);
 
-    res.send(game);
+    res.json(game);
   });
 
   app.post("/api/games", async (req, res) => {
+    const game = await new Game().save();
+
+    res.json(game);
+  });
+
+  app.patch("/api/games/:id", async (req, res) => {
     const { player } = req.body;
+    const { id } = req.params;
 
-    const game = await new Game({
-      players: [player]
-    }).save();
+    const game = await Game.findByIdAndUpdate(
+      id,
+      { $push: { players: player } },
+      { new: true }
+    );
 
-    res.send(game);
+    res.json(game);
   });
 };
